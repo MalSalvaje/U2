a564c54060caf27cb9aafe4d4a08461582ff38e8
saganis <saganis@sauvage.com>
2022-05-08 00:50:13 -0400

    Iniciando ejercicio 6

---
+++ ejercicio6/manejador.py
@@ -0,0 +1,14 @@
+from claseviajero import Viajero
+
+class Manejador:
+    __lista= []
+    def __init__:
+        self.__lista=[]
+    def __gt__(op1, op2):
+        if isinstance(viajero1, viajero2, Viajero):
+            if viajero1.getMillas()>viajero2.getMillasi():
+                return True
+            else:
+            return False
+        else:
+            print('No puede realizarse la comparación.')
