824ec557385fb96f08130f03d62e094aa9ec4743
saganis <saganis@sauvage.com>
2022-05-09 11:47:02 -0400

    Correccion clase Medicamento.

---
+++ integrador/clasemedicamento.py
@@ -0,0 +1,40 @@
+class Medicamento:
+    __idCama = None
+    __idMedicamento = None
+    __nombreComercial = None
+    __monodroga = None
+    __presentacion = None
+    __cantidadAplicada = None
+    __precio= 0
+
+    def __init__(self, idCama, idMedicamento, nombreComercial, monodroga, presentacion, cantidadAplicada, precioTotal):
+        self.__idCama = idCama
+        self.__idMedicamento= idMedicamento
+        self.__nombreComercial = nombreComercial
+        self.__monodroga = monodroga
+        self.__presentacion = presentacion
+        self.__cantidadAplicada = cantidadAplicada
+        if type(precio)==float:
+            self.__precio= precioTotal
+    
+    
+    def getIdCama(self):
+        return self.__idCama
+    
+    def getIdMedicamento(self):
+        return self.__idMedicamento
+    
+    def getNombreComercial(self):
+        return self.__nombreComercial
+    
+    def getMonodroga(self):
+        return self.__monodroga
+    
+    def getPresentacion(self):
+        return self.__presentacion
+    
+    def getCantidadAplicada(self):
+        return self.__cantidadAplicada
+    
+    def getPrecio(self):
+        return self.__precio
